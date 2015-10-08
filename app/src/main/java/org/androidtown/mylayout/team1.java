6c64eb60918dc995ee700eae211c780934a45aea
hyeongeon <puheh@nate.com>
2015-10-08 16:45:14 +0900

    거의다함

--- app/src/main/java/org/androidtown/mylayout/team1.java
+++ app/src/main/java/org/androidtown/mylayout/team1.java
@@ -4,7 +4,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 
 import java.io.IOException;
@@ -13,7 +13,7 @@ import java.io.IOException;
  * Created by Administrator on 2015-09-17.
  */
 public class team1 extends AppCompatActivity {
-    Button button;
+    ImageButton button;
     SeekBar seekbar;
     MediaPlayer music;
 
@@ -24,37 +24,14 @@ public class team1 extends AppCompatActivity {
 
         music = MediaPlayer.create(this, R.raw.leeyongkyu);
         music.setLooping(false);
-        button = (Button) findViewById(R.id.button1);
-        seekbar = (SeekBar) findViewById(R.id.seekBar1);
-
-        seekbar.setMax(music.getDuration());
+        button = (ImageButton) findViewById(R.id.imageButton);
 
 
-
-        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-            }
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress,
-                                          boolean fromUser) {
-                // TODO Auto-generated method stub
-                if(fromUser)
-                    music.seekTo(progress);
-            }
-        });
 
 
 
     }
-    public void button(View v){
+    public void button(View v) {
         if(music.isPlaying()){
 // 재생중이면 실행될 작업 (정지)
             music.stop();
@@ -65,36 +42,13 @@ public class team1 extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            music.seekTo(0);
 
-            button.setText("start");
-            seekbar.setProgress(0);
         }else{
             music.start();
-            button.setText("stop");
 
-            Thread();
         }
     }
-    public void Thread(){
-        Runnable task = new Runnable(){
-            public void run(){
 
-                while(music.isPlaying()){
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-
-                        e.printStackTrace();
-                    }
-
-                    seekbar.setProgress(music.getCurrentPosition());
-                }
-            }
-        };
-        Thread thread = new Thread(task);
-        thread.start();
-    }
     private void killPlayer(){
         if(music!=null){
             music.release(); //
