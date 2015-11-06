b958e1348bf9668381dcd915d9047364d7d7c28f
hyeongeon <puheh@nate.com>
2015-11-06 15:43:06 +0900

    nc & musicmaker
    f

--- app/src/main/java/org/androidtown/mylayout/team6.java
+++ app/src/main/java/org/androidtown/mylayout/team6.java
@@ -1,16 +1,68 @@
 package org.androidtown.mylayout;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Administrator on 2015-10-01.
  */
 public class team6 extends AppCompatActivity{
 
+    MediaPlayer music=new MediaPlayer();
+    int count=0;
+    MusicMaker m=new MusicMaker();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team6);
+
+    }
+
+    public void Button61(View v){
+        //MusicMaker m=new MusicMaker();
+        music=m.play(music,"jisukhun",this,count);
+        count=m.count();
+
+    }
+
+    public void Button62(View v){
+        music=m.play(music,"kimjongho",this,count);
+        count=m.count();
+    }
+    public void Button63(View v){
+        music=m.play(music,"kimtaegun",this,count);
+        count=m.count();
+    }
+    public void Button64(View v){
+        music=m.play(music,"leehojun",this,count);
+        count=m.count();
+    }
+    public void Button65(View v){
+        music=m.play(music,"leejongwook",this,count);
+        count=m.count();
+    }
+    public void Button66(View v){
+        music=m.play(music,"nasungbeom",this,count);
+        count=m.count();
+    }
+    public void Button67(View v){
+        music=m.play(music,"parkminwoo",this,count);
+        count=m.count();
+    }
+    public void Button68(View v){
+        music=m.play(music,"sonsiheon",this,count);
+        count=m.count();
+    }
+    public void Button69(View v){
+        music=m.play(music,"thames",this,count);
+        count=m.count();
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        m.killPlayer();
     }
 }
--- app/src/main/java/org/androidtown/mylayout/team8.java
+++ app/src/main/java/org/androidtown/mylayout/team8.java
@@ -1,16 +1,81 @@
 package org.androidtown.mylayout;
 
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 /**
  * Created by Administrator on 2015-10-01.
  */
+
 public class team8 extends AppCompatActivity {
+    SoundPool pool;
+    int a;
+    MediaPlayer music;
+    int count=0;
+    MediaPlayer out;
+    MediaPlayer remember;
+    //MusicPlayer p= new MusicPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team8);
+
+        out=MediaPlayer.create(this,R.raw.najiwan);
+
     }
+
+    public void Button82(View v){
+        //count++;
+        MusicPlayer p=new MusicPlayer();
+
+        int addr;
+        if(out.isPlaying()){
+            count=1;
+            remember= p.musicplay(out,count);
+            count++;
+            return;
+        }
+
+        addr=p.play(music,"leeyonggyu",this);
+        out=MediaPlayer.create(this,addr);
+        if(count==0)remember=p.musicplay(out,count);
+        if(count==2)out=p.musicplay(remember,count);
+
+        //out= p.play(music,"leeyonggyu",this);
+       // if(out.isPlaying()){
+       //     p.musicstop(out);
+       // }
+    }
+
+    protected void killPlayer(){
+        if(out!=null){
+            out.release();
+            out=null;
+        }
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        killPlayer();
+    }
+
+
+
 }
+//getApplicationContext()
+/*   public void Button81(View v) {
+        pool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        a = pool.load(this, R.raw.joohyungsang, 1);
+        pool.play(a, 1.0f, 1.0f, 0, 0, 1.0f);
+        int waitLimit=1000;
+        int waitCounter=0;
+        int throttle=10;
+        while(pool.play(a, 1.0f, 1.0f, 0, 0, 1.0f)==0&&waitCounter<waitLimit){
+            waitCounter++;
+            SystemClock.sleep((throttle));
+        }
+    }*/
